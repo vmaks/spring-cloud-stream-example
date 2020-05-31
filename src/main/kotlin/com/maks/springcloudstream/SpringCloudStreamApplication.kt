f19f8b0aa53314db33519bc1b4dc42a0c389c6cb
Maksim Vlasov <vlasov.maksim@hotmail.com>
2020-05-31 23:09:17 +0300

    Add empty project

---
+++ src/main/kotlin/com/maks/springcloudstream/SpringCloudStreamApplication.kt
@@ -0,0 +1,11 @@
+package com.maks.springcloudstream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudStreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudStreamApplication>(*args)
+}
