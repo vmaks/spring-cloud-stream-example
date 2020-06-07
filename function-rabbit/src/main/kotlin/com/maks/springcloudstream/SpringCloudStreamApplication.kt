69636bf9857f390f651debd9e6292cdbe6237186
Maksim Vlasov <vlasov.maksim@hotmail.com>
2020-06-07 18:20:44 +0300

    Refactor

--- function-rabbit/src/main/kotlin/com/maks/springcloudstream/SpringCloudStreamApplication.kt
+++ function-rabbit/src/main/kotlin/com/maks/springcloudstream/SpringCloudStreamApplication.kt
@@ -2,7 +2,6 @@ package com.maks.springcloudstream
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.stream.annotation.EnableBinding
 
 @SpringBootApplication
 class SpringCloudStreamApplication
